972024adcc19ca47571aa71877396a28bee3f319
GangHuo <993610942@qq.com>
2020-08-10 16:24:27 +0800

    add mybatis-plus PaginationInterceptor

--- share-core/src/main/java/cool/likeu/share/config/MybatisConfig.java
+++ share-core/src/main/java/cool/likeu/share/config/MybatisConfig.java
@@ -1,9 +1,19 @@
 package cool.likeu.share.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan({"cool.likeu.share.mapper"})
+@ConditionalOnClass(value = {PaginationInterceptor.class})
 public class MybatisConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
 }
