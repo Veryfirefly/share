1d40e919df748b62fe9085d54640a002068d2e44
GangHuo <993610942@qq.com>
2020-08-10 13:32:58 +0800

    initial commit

---
+++ src/test/java/cool/likeu/share/ShareApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.likeu.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
