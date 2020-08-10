38416d8b1a4f05791b2a6455741b91ac29f70e89
GangHuo <993610942@qq.com>
2020-08-10 13:55:40 +0800

    added share-core module and modify pom

---
+++ share-core/src/test/java/cool/likeu/share/JunitShareApplication.java
@@ -0,0 +1,15 @@
+package cool.likeu.share;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+public class JunitShareApplication {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
